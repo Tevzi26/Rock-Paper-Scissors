06eee7ec509066fe2c9889dfb4e5e87f64b5e1dd
Tevzi26 <knez@Ideapad5>
2023-06-05 18:29:47 +0200

    final commit

--- main.js
+++ main.js
@@ -2,12 +2,32 @@ function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
     if (choice == 0) {
-        return "Rock";
+        return "rock";
     } else if (choice == 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 };
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie"
+    } else if ((playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "paper" && computerSelection == "rock") || 
+        (playerSelection == "scissors" && computerSelection == "paper")) 
+    {
+        return "Player wins"
+    } else {
+        return "Computer wins"
+    }
+
+
+
+};
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection))
